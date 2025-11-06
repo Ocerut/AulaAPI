c9980cdaaa367a51229f3957eb4a2a6ab0471d46
Potato <00121046@unisoacad.uniso.br>
2025-11-05 22:32:14 -0300

    05/11 apiproj

---
+++ API_Aula/API_Aula/Model/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Aula.Model
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Vida { get; set; }
+        public string QtdeItens { get; set; }
+        public string PosX { get; set; }
+        public string PosY { get; set; }
+        public string PosZ { get; set; }
+    }
+}
